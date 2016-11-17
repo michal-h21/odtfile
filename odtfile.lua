d883841925a498d006589ce64c3206b61f941bef
Michal Hoftich <michal.h21@gmail.com>
2016-11-17 23:47:31 +0100

    Just playing

--- odtfile.lua
+++ odtfile.lua
@@ -14,8 +14,85 @@ local function odtfile(filename)
   local contentdom = dom.parse(content)
   local used_attrs = {}
   local used_elements = {}
+  local path = contentdom:get_path("office:document-content office:body office:text")
+  local child = contentdom
+  if path then child = path[1] end
 
-  contentdom:traverse_elements(function(el)
+  local buffer = {}
+
+  local function mytraverse(el)
+    local env_stack = {}
+    local function add_buffer(text)
+      buffer[#buffer+1]=text
+    end
+    local function add_command(name)
+      add_buffer '\\'
+      add_buffer(name)
+      add_buffer "{"
+    end
+
+    local function close_command()
+      add_buffer "}"
+    end
+
+    local function begin(name)
+      add_command "begin"
+      add_buffer(name)
+      close_command()
+      table.insert(env_stack, name)
+    end
+
+    local function close()
+      add_command "end"
+      local name = table.remove(env_stack)
+      add_buffer(name)
+      close_command()
+    end
+
+
+
+    local function process_children(ch)
+      for _, x in ipairs(ch:get_children()) do
+        mytraverse(x)
+      end
+    end
+      
+    if el:is_element() then
+      local name = el:get_element_name() 
+      if name == "text:p" then
+        add_buffer "\n"
+        process_children(el)
+        add_buffer("\n\n")
+      elseif name == "text:h" then
+        add_buffer '\\section{'
+        process_children(el)
+        add_buffer "}\n"
+      elseif name == "text:list" then
+        begin "itemize"
+        add_buffer "\n"
+        process_children(el)
+        close()
+        add_buffer "\n"
+      elseif name =="text:list-item" then
+        add_buffer '\\item '
+        process_children(el)
+      elseif name == "text:span" then
+        add_command "span"
+        process_children(el)
+        close_command()
+      else
+        add_buffer("["..name.."]")
+        process_children(el)
+      end
+    elseif el:is_text() then
+      add_buffer(el._text)
+    end
+  end
+  mytraverse(child)
+  print(table.concat(buffer))
+        
+
+  child:traverse_elements(function(el)
     local name = el:get_element_name()
     local element = used_elements[name]
     if not element then
@@ -28,7 +105,7 @@ local function odtfile(filename)
     el._name = element.name
     el._ns = element.ns
 
-    print(name, element.name, element.ns)
+    -- print(name, element.name, element.ns)
     for attr, value in pairs(el._attr or {}) do
       if not used_attrs[attr] then
         local schema = attr:match "xmlns:(.+)"
@@ -40,7 +117,7 @@ local function odtfile(filename)
     end
   end)
   ODT.schemas = schemas
-  contentdom:serialize()
+  -- print(contentdom:serialize(child))
   -- print(content)
 end
 
